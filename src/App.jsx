3d2b891839956ec9aea162647c02105d42d2b4df
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-10-11 20:51:16 -0400

    feat: implement header section with logo, search bar, and favorites button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { Header } from "./components/Header";
+
 export function App() {
   return (
     <div className="bg-gray-100 text-gray-900 min-h-screen">
+      <Header />
       <main className="min-h-screen">
         
       </main>
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { Heart, MagnifyingGlass } from "phosphor-react";
+
+export function Header() {
+  return (
+    <header className="border border-b-gray-400">
+      <div className="container mx-auto px-4 py-8 flex justify-between items-center gap-8">
+        <div>
+          <a href="/" aria-label="PicFinder logo, vá para a homepage" className="text-2xl font-bold">
+            PicFinder
+          </a>
+        </div>
+
+        <form className="flex-grow" role="search" aria-label="Busque fotos e ilustrações">
+          <label htmlFor="search" className="sr-only">Busque por fotos</label>
+          <div className="relative">
+            <input 
+              type="text" 
+              id="search" 
+              className="w-full rounded-lg py-2 pl-2 bg-gray-300 placeholder:text-gray-600" 
+              placeholder="Busque fotos e ilustrações" 
+            />
+            <button 
+              type="submit" 
+              className="absolute top-[0.625rem] right-2 text-gray-600" 
+              aria-label="Pesquisar">
+              <MagnifyingGlass size={20} />
+            </button>
+          </div>
+        </form>
+
+        <nav aria-label="Navegação principal">
+          <ul className="flex items-center gap-4">
+            <li>
+              <button 
+                className="flex items-center gap-1" 
+                aria-label="Veja suas imagens favoritadas"
+              >
+                Favoritas 
+                <Heart size={20} weight="fill" className="text-red-700" />
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
