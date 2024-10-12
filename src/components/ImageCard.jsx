6ba77d784ce7c48df7854bcdd6f3ba8f2e7369b1
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-10-12 10:42:56 -0400

    feat: implement ImageCard component

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,28 @@
+export function ImageCard({ image }) {
+  return (
+    <div key={image.id} className="relative overflow-hidden group">
+      <img
+        src={image.src}
+        alt={image.alt}
+        loading="lazy"
+        className="
+                object-cover w-full h-full rounded-lg 
+                transition-transform duration-400 
+                group-hover:scale-110 group-hover:grayscale aspect-square"
+      />
+      <div
+        className="
+                absolute cursor-pointer inset-0 p-4 
+                bg-black text-white bg-opacity-50 
+                flex flex-col items-center justify-center 
+                opacity-0 transition-opacity duration-300
+                group-hover:opacity-100"
+      >
+        <p>{image.author}</p>
+        <a href="#" className="underline mt-2">
+          Ver detalhes
+        </a>
+      </div>
+    </div>
+  );
+}
